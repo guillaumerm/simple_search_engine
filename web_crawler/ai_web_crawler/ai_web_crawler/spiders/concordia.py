93457f7450fa024fedc43e1a538f7d9d18f11cf5
Guillaume Rochefort-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2019-11-30 12:04:11 +0000

    Removed the resource that was creating a loop

--- web_crawler/ai_web_crawler/ai_web_crawler/spiders/concordia.py
+++ web_crawler/ai_web_crawler/ai_web_crawler/spiders/concordia.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy.linkextractors import LinkExtractor
 
 
@@ -7,12 +8,14 @@ class ConcordiaSpider(scrapy.Spider):
     name = 'concordia'
     allowed_domains = ['concordia.ca']
     start_urls = ['https://www.concordia.ca/research.html']
-    number = 1
+    number = 0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def parse(self, response):
+        ConcordiaSpider.number += 1
+
         # Get the main content of the website that is ignoring
         # the navigation bar and the footer
         main_content = response.xpath('//section[@id="content-main"]')
@@ -31,14 +34,18 @@ class ConcordiaSpider(scrapy.Spider):
             file.write(content)
             file.flush()
 
-        ConcordiaSpider.number += 1
-
         with open('../../corpora/concordia/links/links', 'a') as file:
             file.write(str(ConcordiaSpider.number-1) + ':' + response.url + '\n')
             file.flush()
 
         # Make sure to extract only text from important pages
-        link_extractor = LinkExtractor(allow=('.*news.*', '.*research.*', '.*next-gen.*'), deny=('https://www.concordia.ca/research/lifestyle-addiction/tools/scientific-monitoring.html'), allow_domains=self.allowed_domains, unique=True, strip=True)
+        link_extractor = LinkExtractor(
+            allow=('.*news.*', '.*research.*', '.*next-gen.*'), 
+            deny=('.*/lifestyle-addiction/tools/scientific-monitoring.html.*'), 
+            allow_domains=self.allowed_domains, 
+            unique=True, 
+            strip=True
+        )
         
         # Check before extracting the links
         for link in link_extractor.extract_links(response):
